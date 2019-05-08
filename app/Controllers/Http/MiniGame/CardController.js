203fb51fe92362c1281da1fa744ffa3c691ab12b
tranphuquy19 <tranphuquy19@gmail.com>
2019-05-08 15:10:55 +0700

    random question

--- app/Controllers/Http/MiniGame/CardController.js
+++ app/Controllers/Http/MiniGame/CardController.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const Card = use('App/Models/Card')
+var shuffle = require('shuffle-array')
 
 class CardController {
     async show({view, params}){
-        let data = JSON.parse(Card.find(params.id))
+        const data = await Card.find(params.id)
+        var questions = []
+        questions = JSON.parse(data.content)
+        shuffle(questions)
+        return questions.slice(0, 49)
     }
 }
 
